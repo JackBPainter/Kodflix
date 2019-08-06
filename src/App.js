7413a440a0262ed3f58ea61f1ff304b65af8613d
Jack Painter <littlesympathy@E-fly-MBP.localdomain>
2019-08-06 12:01:55 +0100

    #3 Added 6 TV show titles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import TheShiningCover from './The Shining Movie Cover.jpg';
+import TheShiningCover from './images/The Shining Movie Cover.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={TheShiningCover} alt="Shining Movie Cover" />
+        <img src={TheShiningCover} alt="The Shining Movie"/>
         <p>
           <h1>Welcome to Jacks Kodlfix!</h1>
         </p>
-      </header>
+        </header>
+        <br /><br />
+        <div className='wrapper'>  
+          <div className='item'>Black Mirror</div>
+          <div className='item'>F1 Drive to Survive</div>
+          <div className='item'>Hannibal</div>
+        </div>
+        <div className='wrapper'>  
+          <div className='item'>Last Chance U</div>
+          <div className='item'>Louis' Weird Weekends</div>
+          <div className='item'>Peaky Blinders</div>
+        </div>
     </div>
   );
 }
