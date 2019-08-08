3bf0e16dbe53fbf074063088573c058fe6253b46
Jack Painter <littlesympathy@E-fly-MBP.localdomain>
2019-08-08 11:17:23 +0100

    #4 Single TV show on each row for mobile devices

--- src/App.js
+++ src/App.js
@@ -19,23 +19,41 @@ function App() {
         <div className='wrapper'>  
           <div className='item'>
           <img src={BlackMirrorCover} alt="Black Mirror" />
+          <div className='coverNames'>
+            <h1> Black Mirror </h1>
+          </div>
           </div>
           <div className='item'>
           <img src={GameofThrones} alt="Game of Thrones"/>
+          <div className='coverNames'>
+            <h1> Game of Thrones </h1>
+          </div>
           </div>
           <div className='item'>
           <img src={HannibalCover} alt="Hannibal"/>
+          <div className='coverNames'>
+            <h1> Hannibal </h1>
+          </div>
           </div>
         </div>
         <div className='wrapper'>  
           <div className='item'>
           <img src={LastChanceUCover} alt="Last Chance U"/>
+          <div className='coverNames'>
+            <h1> Last Chance U </h1>
+          </div>
           </div>
           <div className='item'>
           <img src={PeakyBlindersCover} alt="Peaky Blinders"/>
+          <div className='coverNames'>
+            <h1> Peaky Blinders </h1>
+          </div>
           </div>
           <div className='item'>
           <img src={LouisWeirdWeekendsCover} alt="Louis Theroux Weird Weekends"/>
+          <div className='coverNames'>
+            <h1> Louis' Weird Weekends </h1>
+          </div>
           </div>
         </div>
     </div>
