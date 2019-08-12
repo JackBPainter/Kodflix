4fdaa44e7ee960f82bca72d7702252ba1e4c8cb3
Jack Painter <littlesympathy@E-fly-MBP.localdomain>
2019-08-12 17:47:58 +0100

    #9 Created a Gallery component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import TvCover from './TvCover';
-import BlackMirrorCover from './images/Black Mirror Cover.jpg';
-import GameofThrones from './images/Game of Thrones Cover.jpg';
-import HannibalCover from './images/Hannibal Cover.jpg';
-import LastChanceUCover from './images/Last Chance U Cover.jpg';
-import PeakyBlindersCover from './images/Peaky Blinders Cover.jpg';
-import LouisWeirdWeekendsCover from './images/Louis Weird Weekends Cover.jpg';
+import Gallery from './Gallery';
 import './App.css';
 
 export default function App() {
@@ -17,17 +11,7 @@ export default function App() {
         </p>
         </header>
         <br /><br />
-        <div className='wrapper'>  
-          <TvCover tvshow='Black Mirror' image = {BlackMirrorCover}/>
-          <TvCover tvshow='Game of Thrones' image = {GameofThrones}/>
-          <TvCover tvshow='Hannibal' image = {HannibalCover}/>
-
-        <div className='wrapper'>  
-          <TvCover tvshow='Last Chance U' image = {LastChanceUCover}/>
-          <TvCover tvshow='Peaky Blinders' image = {PeakyBlindersCover}/>
-          <TvCover tvshow='Louis Weird Weekends' image = {LouisWeirdWeekendsCover}/>
-          </div>
-          </div>
+        <Gallery />
         </div>
   );
 }
