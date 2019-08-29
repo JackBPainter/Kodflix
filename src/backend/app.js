bc6d8a17734f56bf1cf1fb050a767d5058f13f65
Jack Painter <littlesympathy@E-fly-MBP.localdomain>
2019-08-29 20:19:17 +0100

    #19 Return a list of shows from the backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const tvShows = require('./tvShows.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/tvShows', (req, res) => res.send(tvShows()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/backend/tvShows.js
@@ -0,0 +1,41 @@
+function getGallery() {
+    return [
+      {
+        title: "Black Mirror",
+        id: "black-mirror",
+        synopsis:
+          "An anthology series exploring a twisted, high-tech world where humanity's greatest innovations and darkest instincts collide."
+      },
+      {
+        title: "Game of Thrones",
+        id: "game-of-thrones",
+        synopsis:
+          "Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years."
+      },
+      {
+        title: "Hannibal",
+        id: "hannibal",
+        synopsis:
+          "Explores the early relationship between the renowned psychiatrist and his patient, a young FBI criminal profiler, who is haunted by his ability to empathize with serial killers."
+      },
+      {
+        title: "Last Chance U",
+        id: "last-chance-u",
+        synopsis:
+          "Intense look inside the world of junior college football, chronicling the stories of players and coaches in the classroom and on the field."
+      },
+      {
+        title: "Peaky Blinders",
+        id: "peaky-blinders",
+        synopsis:
+          "A gangster family epic set in 1919 Birmingham, England; centered on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby."
+      },
+      {
+        title: "Louis Theroux Weird Weekends",
+        id: "louis-weird-weekends",
+        synopsis:
+          "Louis Theroux documents some of the US's most bizarre happenings, events and rituals, by getting involved himself."
+      }
+    ];
+  }
+module.exports = getGallery;
