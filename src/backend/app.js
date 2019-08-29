c91bc37d355254ff8bfd394b4cdf0999f39a5458
Jack Painter <littlesympathy@E-fly-MBP.localdomain>
2019-08-29 20:59:12 +0100

    #20 Improved setup of backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const tvShows = require('./tvShows.js');
 
 app.get('/rest/tvShows', (req, res) => res.send(tvShows()));
+app.get('/', (req, res) => res.send('Please enter the correct URL (/localhost3001/rest/tvShows'));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
