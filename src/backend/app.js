4bb6d9db6e51a98a95ad66b054da6e3ad5454aa9
Jack Painter <littlesympathy@E-fly-MBP.localdomain>
2019-09-03 19:40:56 +0100

    #23 Enable the backend server in production

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = process.env.PORT || 3001;
 const getGallery = require('./tvShows.js');
@@ -7,9 +8,9 @@ app.get('/', (req, res) => res.send('Please enter the correct URL (/localhost300
 app.get('/rest/tvShows', (req, res) => res.send(getGallery()));
 app.use(express.static(path.join(__dirname, '../../build')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build/', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-//express.static is a built in express middleware middleware function which is used to serve static files (images, css files and JS files). 
+//express.static is a built in express middleware middleware function which is used to serve static files (images, css files and js files)
