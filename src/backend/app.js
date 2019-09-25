3f0203e8eeee2c774845ea39f64646ac69b0c301
Jack Painter <littlesympathy@Danzig.localdomain>
2019-09-25 17:22:32 +0100

    Re-order route route from express

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 const TvShows = require("./tvShows.js");
 
-app.get('/', (req, res) => res.send('Please enter the correct URL (/localhost3001/rest/tvShows'));
-app.get("/rest/tvShows", (req, res) => res.send(TvShows()));
 app.use(express.static(path.join(__dirname, "../../build")));
+
+app.get("/rest/tvShows", (req, res) => res.send(TvShows()));
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../../build/", "index.html"));
 });
